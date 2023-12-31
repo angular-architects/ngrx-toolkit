ca67726e5818460fb35bd85211a1267e43b6e07a
bohoffi <bo.hoffi@gmail.com>
2023-12-31 12:21:42 +0100

    feat: add storage sync

--- apps/demo/src/app/app.routes.ts
+++ apps/demo/src/app/app.routes.ts
@@ -5,13 +5,20 @@ import { FlightSearchSimpleComponent } from './flight-search-data-service-simple
 import { FlightEditSimpleComponent } from './flight-search-data-service-simple/flight-edit-simple.component';
 import { FlightSearchDynamicComponent } from './flight-search-data-service-dynamic/flight-search.component';
 import { FlightEditDynamicComponent } from './flight-search-data-service-dynamic/flight-edit.component';
+import { TodoStorageSyncComponent } from './todo-storage-sync/todo-storage-sync.component';
 
 export const appRoutes: Route[] = [
   { path: 'todo', component: TodoComponent },
   { path: 'flight-search', component: FlightSearchComponent },
-  { path: 'flight-search-data-service-simple', component: FlightSearchSimpleComponent },
+  {
+    path: 'flight-search-data-service-simple',
+    component: FlightSearchSimpleComponent,
+  },
   { path: 'flight-edit-simple/:id', component: FlightEditSimpleComponent },
-  { path: 'flight-search-data-service-dynamic', component: FlightSearchDynamicComponent },
+  {
+    path: 'flight-search-data-service-dynamic',
+    component: FlightSearchDynamicComponent,
+  },
   { path: 'flight-edit-dynamic/:id', component: FlightEditDynamicComponent },
-
+  { path: 'todo-storage-sync', component: TodoStorageSyncComponent },
 ];
