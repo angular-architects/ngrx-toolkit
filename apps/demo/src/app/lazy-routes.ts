729c84d9198ac4f977d7c52f6ac14f484ca20404
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2024-05-13 17:36:48 +0200

    fix: lazy load routes to get under the build budget

---
+++ apps/demo/src/app/lazy-routes.ts
@@ -0,0 +1,31 @@
+import { Route } from '@angular/router';
+import { TodoComponent } from './todo/todo.component';
+import { FlightSearchComponent } from './flight-search/flight-search.component';
+import { FlightSearchSimpleComponent } from './flight-search-data-service-simple/flight-search-simple.component';
+import { FlightEditSimpleComponent } from './flight-search-data-service-simple/flight-edit-simple.component';
+import { FlightSearchDynamicComponent } from './flight-search-data-service-dynamic/flight-search.component';
+import { FlightEditDynamicComponent } from './flight-search-data-service-dynamic/flight-edit.component';
+import { TodoStorageSyncComponent } from './todo-storage-sync/todo-storage-sync.component';
+import { provideFlightStore } from './flight-search-redux-connector/+state/redux';
+import { FlightSearchReducConnectorComponent } from './flight-search-redux-connector/flight-search.component';
+
+export const lazyRoutes: Route[] = [
+  { path: 'todo', component: TodoComponent },
+  { path: 'flight-search', component: FlightSearchComponent },
+  {
+    path: 'flight-search-data-service-simple',
+    component: FlightSearchSimpleComponent,
+  },
+  { path: 'flight-edit-simple/:id', component: FlightEditSimpleComponent },
+  {
+    path: 'flight-search-data-service-dynamic',
+    component: FlightSearchDynamicComponent,
+  },
+  { path: 'flight-edit-dynamic/:id', component: FlightEditDynamicComponent },
+  { path: 'todo-storage-sync', component: TodoStorageSyncComponent },
+  {
+    path: 'flight-search-redux-connector',
+    providers: [provideFlightStore()],
+    component: FlightSearchReducConnectorComponent,
+  },
+];
