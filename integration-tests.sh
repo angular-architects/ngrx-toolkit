018b96948ffd3e96381086a707eba15f0a52086d
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2024-07-08 01:40:33 +0200

    ci: add integration tests

---
+++ integration-tests.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# This uses different NgRx versions and verifies that the demo app builds.
+
+set -e
+
+echo 'checking against different @ngrx/signals versions'
+
+declare -a versions=('18.0.0-rc.0', '18.0.0-rc.1', '18.0.0-rc.2')
+
+for version in ${versions[*]}; do
+  npm install @ngrx/signals@$version
+  npm run build
+done
