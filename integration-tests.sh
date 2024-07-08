d48f68e6a7c9e72038363015bcf71b1f42535812
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2024-07-08 02:01:09 +0200

    ci: fix integration tests

--- integration-tests.sh
+++ integration-tests.sh
@@ -9,7 +9,6 @@ echo 'checking against different @ngrx/signals versions'
 declare -a versions=('18.0.0-rc.0' '18.0.0-rc.1' '18.0.0-rc.2')
 
 for version in ${versions[*]}; do
-  echo "npm i @ngrx/signals@$version"
   npm i @ngrx/signals@$version
-  npm run build
+  npx nx build --project demo
 done
