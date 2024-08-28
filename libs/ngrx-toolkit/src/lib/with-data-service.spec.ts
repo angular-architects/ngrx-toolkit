d1a214c22b2f8fb2ed2a63ea1b5fd7ac2743eead
Michael Small <chickenhat22@gmail.com>
2024-08-27 23:51:34 -0500

    test(dataService): add `loadById` test (no prefix), and note on test expecations

--- libs/ngrx-toolkit/src/lib/with-data-service.spec.ts
+++ libs/ngrx-toolkit/src/lib/with-data-service.spec.ts
@@ -1,12 +1,16 @@
+import { Injectable } from '@angular/core';
+import { fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { Observable, firstValueFrom, of } from 'rxjs';
 import { signalStore } from '@ngrx/signals';
 import { withEntities } from '@ngrx/signals/entities';
-import { Injectable } from '@angular/core';
-import { Observable, firstValueFrom, of } from 'rxjs';
 import { EntityId } from '@ngrx/signals/entities';
-import { fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { withCallState } from './with-call-state';
 import { DataService, withDataService } from './with-data-service';
 
+// Since the resulting shape of entities in the store is a matter of the implementing services of `dataServiceType`,
+//     these tests are more so about verifying that each resulting method exists, with or without prefixes.
+// The expectations on the resulting shape of the data in the store following these tests merely asserts
+//     that the store was patched in the right generic shape and with respective call states.
 describe('withDataService', () => {
   it('should load from a service and set entities in the store', fakeAsync(() => {
     TestBed.runInInjectionContext(() => {
@@ -30,9 +34,37 @@ describe('withDataService', () => {
       expect(store.entities().length).toBe(1);
     });
   }));
+  it('should load by ID from a service and set entities in the store', fakeAsync(() => {
+    TestBed.runInInjectionContext(() => {
+      const Store = signalStore(
+        { providedIn: 'root' },
+        withCallState(),
+        withEntities<Flight>(),
+        withDataService({
+          dataServiceType: MockFlightService,
+          filter: { from: 'Paris', to: 'New York' },
+        })
+      );
+      const store = new Store();
+
+      tick(1);
+
+      store.loadById(2);
+
+      tick(1);
+
+      expect(store.current()).toEqual({
+        id: 2,
+        from: 'Paris',
+        to: 'New York',
+        date: new Date().toDateString(),
+        delayed: false,
+      });
+    });
+  }));
 });
 
-export type FlightFilter = {
+type FlightFilter = {
   from: string;
   to: string;
 };
@@ -40,7 +72,7 @@ export type FlightFilter = {
 @Injectable({
   providedIn: 'root',
 })
-export class MockFlightService implements DataService<Flight, FlightFilter> {
+class MockFlightService implements DataService<Flight, FlightFilter> {
   loadById(id: EntityId): Promise<Flight> {
     return firstValueFrom(this.findById('' + id));
   }
@@ -68,7 +100,6 @@ export class MockFlightService implements DataService<Flight, FlightFilter> {
   }
 
   private find(from: string, to: string, urgent = false): Observable<Flight[]> {
-    console.log('hey', from, to);
     return of([
       {
         id: 1,
@@ -105,7 +136,7 @@ export class MockFlightService implements DataService<Flight, FlightFilter> {
   }
 }
 
-export type Flight = {
+type Flight = {
   id: number;
   from: string;
   to: string;
