611ad0d08d0e348494345b3b8cd2062e46b14526
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2025-01-06 13:07:35 +0100

    docs: add social card

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -64,8 +64,7 @@ const config: Config = {
   ],
 
   themeConfig: {
-    // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/social.jpg',
     navbar: {
       title: 'NgRx Toolkit',
       logo: {
