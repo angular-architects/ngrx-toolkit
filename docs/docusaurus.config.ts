bc09076ae4af308f7f40fc98d4a414684a4832b2
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2025-01-10 16:17:38 +0100

    Update docusaurus.config.ts

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
   url: 'https://angular-architects.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/ngrx-toolkit',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
