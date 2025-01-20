064a20b039ff0cbc1162216863f6cac92f8bcefa
mzkmnk <sma0729mex@gmail.com>
2025-01-20 22:39:42 +0900

    refactor(with-indexeddb): initialize read and write order

--- apps/demo/src/app/todo-storage-sync/synced-todo-store.ts
+++ apps/demo/src/app/todo-storage-sync/synced-todo-store.ts
@@ -1,4 +1,4 @@
-import { patchState, signalStore, withMethods } from '@ngrx/signals';
+import { patchState, signalStore, withHooks, withMethods } from '@ngrx/signals';
 import {
   withEntities,
   setEntity,
@@ -17,7 +17,8 @@ export const SyncedTodoStore = signalStore(
   withStorageSync(
     {
       dbName: 'ngrx-toolkit',
-      storeName: 'todos',
+      storeName: 'todo',
+      parse: (value: string) => JSON.parse(value),
     },
     withIndexedDB()
   ),
@@ -40,5 +41,43 @@ export const SyncedTodoStore = signalStore(
         );
       },
     };
+  }),
+  withHooks({
+    onInit(store) {
+      store.add({
+        name: 'Go for a Walk',
+        finished: false,
+        description:
+          'Go for a walk in the park to relax and enjoy nature. Walking is a great way to clear your mind and get some exercise. It can help reduce stress and improve your mood. Make sure to wear comfortable shoes and bring a bottle of water. Enjoy the fresh air and take in the scenery around you.',
+      });
+
+      store.add({
+        name: 'Read a Book',
+        finished: false,
+        description:
+          'Spend some time reading a book. It can be a novel, a non-fiction book, or any other genre you enjoy. Reading can help you relax and learn new things.',
+      });
+
+      store.add({
+        name: 'Write a Journal',
+        finished: false,
+        description:
+          'Take some time to write in your journal. Reflect on your day, your thoughts, and your feelings. Journaling can be a great way to process emotions and document your life.',
+      });
+
+      store.add({
+        name: 'Exercise',
+        finished: false,
+        description:
+          'Do some physical exercise. It can be a workout, a run, or any other form of exercise you enjoy. Exercise is important for maintaining physical and mental health.',
+      });
+
+      store.add({
+        name: 'Cook a Meal',
+        finished: false,
+        description:
+          'Prepare a meal for yourself or your family. Cooking can be a fun and rewarding activity. Try out a new recipe or make one of your favorite dishes.',
+      });
+    },
   })
 );
