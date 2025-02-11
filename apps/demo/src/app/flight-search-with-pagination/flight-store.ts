ad09e314df07b28d79af54b9cd97a756e09e2bf7
Murat Sari <murat.sari@coderabbit.at>
2025-02-11 15:44:06 +0100

    fix: improve tree shaking for with-pagination

--- apps/demo/src/app/flight-search-with-pagination/flight-store.ts
+++ apps/demo/src/app/flight-search-with-pagination/flight-store.ts
@@ -1,30 +1,41 @@
 import { FlightService } from '../shared/flight.service';
-
-import { signalStore, type } from '@ngrx/signals';
-
+import { patchState, signalStore, type, withMethods } from '@ngrx/signals';
 import { withEntities } from '@ngrx/signals/entities';
 import {
   withCallState,
   withDataService,
   withPagination,
+  setPageSize,
+  gotoPage,
 } from '@angular-architects/ngrx-toolkit';
 import { Flight } from '../shared/flight';
 
+// Name of the collection
+const collectionName = 'flight';
+
 export const FlightBookingStore = signalStore(
   withCallState({
-    collection: 'flight',
+    collection: collectionName,
   }),
   withEntities({
     entity: type<Flight>(),
-    collection: 'flight',
+    collection: collectionName,
   }),
   withDataService({
     dataServiceType: FlightService,
     filter: { from: 'Wien', to: '' },
-    collection: 'flight',
+    collection: collectionName,
   }),
   withPagination({
     entity: type<Flight>(),
-    collection: 'flight',
-  })
+    collection: collectionName,
+  }),
+  withMethods((store) => ({
+    setFlightPageSize: (size: number) => {
+      patchState(store, setPageSize(size, { collection: collectionName }));
+    },
+    gotoFlightPage: (page: number) => {
+      patchState(store, gotoPage(page, { collection: collectionName }));
+    },
+  }))
 );
