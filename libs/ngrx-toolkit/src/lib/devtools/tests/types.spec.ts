784c4f1bb4003b0db5a6aaa5c30ae7b1350a566a
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2025-03-26 11:13:42 +0100

    fix(devtools): set types of `withDevtools` to `EmptyFeatureResult`
    This fixes an issue with SignalStore's used to extend from,
    where `withDevtools` messed up the typing.
    
    Closes: #164, #165

---
+++ libs/ngrx-toolkit/src/lib/devtools/tests/types.spec.ts
@@ -0,0 +1,19 @@
+import { computed } from '@angular/core';
+import { patchState, signalStore, withState } from '@ngrx/signals';
+import { withDevtools } from '../with-devtools';
+
+it('should compile when signalStore is extended from', () => {
+  class CounterStore extends signalStore(
+    { protectedState: false },
+    withState({ count: 0 }),
+    withDevtools('counter-store')
+  ) {
+    readonly myReadonlyProp = 42;
+
+    readonly doubleCount = computed(() => this.count() * 2);
+
+    increment(): void {
+      patchState(this, { count: this.count() + 1 });
+    }
+  }
+});
