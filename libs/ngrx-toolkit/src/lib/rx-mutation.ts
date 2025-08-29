9014ebf1f58377cbe454de154d5139ea8f719322
Manfred Steyer <manfred.steyer@gmx.net>
2025-08-29 21:54:26 +0200

    fix: fix some race conditions for rx-mutate

--- libs/ngrx-toolkit/src/lib/rx-mutation.ts
+++ libs/ngrx-toolkit/src/lib/rx-mutation.ts
@@ -44,34 +44,48 @@ export function rxMutation<P, R>(
   const callCount = signal(0);
   const errorSignal = signal<unknown>(undefined);
 
+  let internalStatus = status();
+
   inputSubject
     .pipe(
       flatten((input) =>
         options.operation(input.param).pipe(
           tap((result: R) => {
             options.onSuccess?.(result, input.param);
-            status.set('success');
-            input.resolve({
-              status: 'success',
-            });
+            internalStatus = 'success';
           }),
           catchError((error: unknown) => {
             options.onError?.(error, input.param);
             const mutationError = error ?? 'Mutation failed';
             errorSignal.set(mutationError);
-            status.set('error');
-            input.resolve({
-              status: 'error',
-              error: mutationError,
-            });
+            internalStatus = 'error';
+
             return of(null);
           }),
           finalize(() => {
             callCount.update((c) => c - 1);
-            if (status() === 'processing') {
+
+            if (callCount() > 0) {
+              // Another call took over (e.g. because of using switchMap)
               input.resolve({
                 status: 'aborted',
               });
+            } else if (internalStatus === 'processing') {
+              // Completion without emitting a value
+              input.resolve({
+                status: 'aborted',
+              });
+            } else if (
+              internalStatus === 'error' ||
+              internalStatus === 'success'
+            ) {
+              status.set(internalStatus);
+              input.resolve({
+                status: internalStatus,
+                error: internalStatus === 'error' ? errorSignal() : undefined,
+              });
+            } else {
+              throw new Error('Unexpected mutation status ' + internalStatus);
             }
           }),
         ),
@@ -82,8 +96,15 @@ export function rxMutation<P, R>(
 
   const mutationFn = (param: P) => {
     return new Promise<MutationResult>((resolve) => {
+      // TODO: Do we find a better way for solving this?
+      if (options.operator?.name === 'exhaustMap' && callCount() > 0) {
+        resolve({ status: 'aborted' });
+        return;
+      }
+
       callCount.update((c) => c + 1);
       status.set('processing');
+      errorSignal.set(undefined);
       inputSubject.next({
         param,
         resolve,
--- libs/ngrx-toolkit/src/lib/with-mutations.spec.ts
+++ libs/ngrx-toolkit/src/lib/with-mutations.spec.ts
@@ -1,6 +1,20 @@
-import { fakeAsync, TestBed, tick } from '@angular/core/testing';
+import {
+  fakeAsync,
+  flushMicrotasks,
+  TestBed,
+  tick,
+} from '@angular/core/testing';
 import { patchState, signalStore, withState } from '@ngrx/signals';
-import { delay, Observable, of, switchMap, throwError } from 'rxjs';
+import {
+  concatMap,
+  delay,
+  exhaustMap,
+  mergeMap,
+  Observable,
+  of,
+  switchMap,
+  throwError,
+} from 'rxjs';
 import { rxMutation } from './rx-mutation';
 import { withMutations } from './with-mutations';
 
@@ -12,8 +26,8 @@ async function asyncTick(): Promise<void> {
   });
 }
 
-function calcDouble(value: number): Observable<number> {
-  return of(value * 2).pipe(delay(1000));
+function calcDouble(value: number, delayInMsec = 1000): Observable<number> {
+  return of(value * 2).pipe(delay(delayInMsec));
 }
 
 function fail(_value: number): Observable<number> {
@@ -88,7 +102,7 @@ describe('mutation', () => {
     });
   }));
 
-  it('rxMutation deals with race conditions', fakeAsync(() => {
+  it('rxMutation deals with race conditions using switchMap', fakeAsync(() => {
     let onSuccessCalls = 0;
     let onErrorCalls = 0;
     const lastOnSuccessParams = {
@@ -128,6 +142,8 @@ describe('mutation', () => {
       expect(store.incrementStatus()).toEqual('processing');
       expect(store.incrementProcessing()).toEqual(true);
 
+      flushMicrotasks();
+
       store.increment(2);
       tick(1000);
 
@@ -145,6 +161,266 @@ describe('mutation', () => {
     });
   }));
 
+  it('rxMutation deals with race conditions using mergeMap', fakeAsync(() => {
+    let onSuccessCalls = 0;
+    let onErrorCalls = 0;
+    const lastOnSuccessParams = {
+      result: -1,
+      params: -1,
+    };
+
+    TestBed.runInInjectionContext(() => {
+      const Store = signalStore(
+        withState({ counter: 3 }),
+        withMutations((store) => ({
+          increment: rxMutation({
+            operation: (value: number) => {
+              return calcDouble(value);
+            },
+            operator: mergeMap,
+            onSuccess: (result, params) => {
+              lastOnSuccessParams.params = params;
+              lastOnSuccessParams.result = result;
+              patchState(store, (state) => ({
+                counter: state.counter + result,
+              }));
+              onSuccessCalls++;
+            },
+            onError: (_result) => {
+              onErrorCalls++;
+            },
+          }),
+        })),
+      );
+
+      const store = new Store();
+
+      store.increment(1);
+      tick(500);
+      store.increment(2);
+      tick(500);
+
+      expect(store.incrementStatus()).toEqual('processing');
+      expect(store.incrementProcessing()).toEqual(true);
+
+      // expect(store.counter()).toEqual(7);
+      expect(onSuccessCalls).toEqual(1);
+      expect(onErrorCalls).toEqual(0);
+      expect(lastOnSuccessParams).toEqual({
+        params: 1,
+        result: 2,
+      });
+
+      tick(500);
+
+      expect(store.incrementStatus()).toEqual('success');
+      expect(store.incrementProcessing()).toEqual(false);
+      expect(store.incrementError()).toEqual(undefined);
+
+      expect(store.counter()).toEqual(9);
+      expect(onSuccessCalls).toEqual(2);
+      expect(onErrorCalls).toEqual(0);
+      expect(lastOnSuccessParams).toEqual({
+        params: 2,
+        result: 4,
+      });
+    });
+  }));
+
+  it('rxMutation deals with race conditions using concatMap', fakeAsync(() => {
+    let onSuccessCalls = 0;
+    let onErrorCalls = 0;
+    const lastOnSuccessParams = {
+      result: -1,
+      params: -1,
+    };
+
+    TestBed.runInInjectionContext(() => {
+      const Store = signalStore(
+        withState({ counter: 3 }),
+        withMutations((store) => ({
+          increment: rxMutation({
+            operation: (param: { value: number; delayInMsec: number }) => {
+              return calcDouble(param.value, param.delayInMsec);
+            },
+            operator: concatMap,
+            onSuccess: (result, params) => {
+              lastOnSuccessParams.params = params.value;
+              lastOnSuccessParams.result = result;
+              patchState(store, (state) => ({
+                counter: state.counter + result,
+              }));
+              onSuccessCalls++;
+            },
+            onError: (_result) => {
+              onErrorCalls++;
+            },
+          }),
+        })),
+      );
+
+      const store = new Store();
+
+      store.increment({ value: 1, delayInMsec: 1000 });
+      tick(500);
+      store.increment({ value: 2, delayInMsec: 100 });
+      tick(500);
+
+      expect(store.incrementStatus()).toEqual('processing');
+      expect(store.incrementProcessing()).toEqual(true);
+
+      // expect(store.counter()).toEqual(7);
+      expect(onSuccessCalls).toEqual(1);
+      expect(onErrorCalls).toEqual(0);
+      expect(lastOnSuccessParams).toEqual({
+        params: 1,
+        result: 2,
+      });
+
+      tick(500);
+
+      expect(store.incrementStatus()).toEqual('success');
+      expect(store.incrementProcessing()).toEqual(false);
+      expect(store.incrementError()).toEqual(undefined);
+
+      expect(store.counter()).toEqual(9);
+      expect(onSuccessCalls).toEqual(2);
+      expect(onErrorCalls).toEqual(0);
+      expect(lastOnSuccessParams).toEqual({
+        params: 2,
+        result: 4,
+      });
+    });
+  }));
+
+  it('rxMutation deals with race conditions using mergeMap and two tasks with different delays', fakeAsync(() => {
+    let onSuccessCalls = 0;
+    let onErrorCalls = 0;
+    const lastOnSuccessParams = {
+      result: -1,
+      params: -1,
+    };
+
+    TestBed.runInInjectionContext(() => {
+      const Store = signalStore(
+        withState({ counter: 3 }),
+        withMutations((store) => ({
+          increment: rxMutation({
+            operation: (param: { value: number; delayInMsec: number }) => {
+              return calcDouble(param.value, param.delayInMsec);
+            },
+            operator: mergeMap,
+            onSuccess: (result, params) => {
+              lastOnSuccessParams.params = params.value;
+              lastOnSuccessParams.result = result;
+              patchState(store, (state) => ({
+                counter: state.counter + result,
+              }));
+              onSuccessCalls++;
+            },
+            onError: (_result) => {
+              onErrorCalls++;
+            },
+          }),
+        })),
+      );
+
+      const store = new Store();
+
+      store.increment({ value: 1, delayInMsec: 1000 });
+      tick(500);
+
+      expect(store.incrementStatus()).toEqual('processing');
+      expect(store.incrementProcessing()).toEqual(true);
+
+      store.increment({ value: 2, delayInMsec: 100 });
+      tick(500);
+
+      expect(store.incrementStatus()).toEqual('success');
+      expect(store.incrementProcessing()).toEqual(false);
+      expect(store.incrementError()).toEqual(undefined);
+
+      expect(store.counter()).toEqual(9);
+      expect(onSuccessCalls).toEqual(2);
+      expect(onErrorCalls).toEqual(0);
+      expect(lastOnSuccessParams).toEqual({
+        params: 1,
+        result: 2,
+      });
+    });
+  }));
+
+  it('rxMutation deals with race conditions using exhaustMap', fakeAsync(() => {
+    let onSuccessCalls = 0;
+    let onErrorCalls = 0;
+    const lastOnSuccessParams = {
+      result: -1,
+      params: -1,
+    };
+
+    TestBed.runInInjectionContext(() => {
+      const Store = signalStore(
+        withState({ counter: 3 }),
+        withMutations((store) => ({
+          increment: rxMutation({
+            operation: (param: { value: number; delayInMsec: number }) => {
+              return calcDouble(param.value, param.delayInMsec);
+            },
+            operator: exhaustMap,
+            onSuccess: (result, params) => {
+              lastOnSuccessParams.params = params.value;
+              lastOnSuccessParams.result = result;
+              patchState(store, (state) => ({
+                counter: state.counter + result,
+              }));
+              onSuccessCalls++;
+            },
+            onError: (_result) => {
+              onErrorCalls++;
+            },
+          }),
+        })),
+      );
+
+      const store = new Store();
+
+      store.increment({ value: 1, delayInMsec: 1000 });
+      tick(500);
+
+      expect(store.incrementStatus()).toEqual('processing');
+      expect(store.incrementProcessing()).toEqual(true);
+
+      store.increment({ value: 2, delayInMsec: 100 });
+      tick(500);
+
+      expect(store.incrementStatus()).toEqual('success');
+      expect(store.incrementProcessing()).toEqual(false);
+      expect(store.incrementError()).toEqual(undefined);
+
+      expect(store.counter()).toEqual(5);
+      expect(onSuccessCalls).toEqual(1);
+      expect(onErrorCalls).toEqual(0);
+      expect(lastOnSuccessParams).toEqual({
+        params: 1,
+        result: 2,
+      });
+
+      tick(500);
+
+      expect(store.incrementStatus()).toEqual('success');
+      expect(store.incrementProcessing()).toEqual(false);
+      expect(store.incrementError()).toEqual(undefined);
+
+      expect(store.counter()).toEqual(5);
+      expect(onSuccessCalls).toEqual(1);
+      expect(onErrorCalls).toEqual(0);
+      expect(lastOnSuccessParams).toEqual({
+        params: 1,
+        result: 2,
+      });
+    });
+  }));
+
   it('rxMutation informs about aborted operations', async () => {
     await TestBed.runInInjectionContext(async () => {
       const Store = signalStore(
@@ -155,7 +431,7 @@ describe('mutation', () => {
               return calcDouble(value);
             },
             operator: switchMap,
-            onSuccess: (result, params) => {
+            onSuccess: (result) => {
               patchState(store, (state) => ({
                 counter: state.counter + result,
               }));
@@ -169,6 +445,9 @@ describe('mutation', () => {
       const p1 = store.increment(1);
       const p2 = store.increment(2);
 
+      expect(store.incrementProcessing()).toEqual(true);
+      expect(store.incrementStatus()).toEqual('processing');
+
       await asyncTick();
 
       const result1 = await p1;
@@ -176,6 +455,8 @@ describe('mutation', () => {
 
       expect(result1.status).toEqual('aborted');
       expect(result2.status).toEqual('success');
+      expect(store.incrementProcessing()).toEqual(false);
+      expect(store.incrementStatus()).toEqual('success');
     });
   });
 });
